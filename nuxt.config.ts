7491446e779d96912665cce62dac88348a6b93c3
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-06-27 01:33:53 +0900

    modify ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true,
   typescript: {
     strict: true
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+// クライアントサイドで取得された時刻
+const clientDate = ref()
+onMounted(() => {
+  clientDate.value = dayjs().toString()
+})
+
+// サーバーサイドで取得された時刻
+const { data: serverDate } = await useAsyncData('date', async () => {
+  const date = dayjs().toString()
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  return date
+})
+</script>
+
 <template>
   <div>
     <h1>
       Hello World!!
     </h1>
+    <p>Server Date: {{ serverDate }}</p>
+    <p>Client Date: {{ clientDate }}</p>
   </div>
 </template>
